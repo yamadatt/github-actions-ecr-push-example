f87a40b3528a0a0ab2bb50fe3b2083e9b7fe5472
yamadatt <yamadatt@gmail.com>
2023-07-19 21:09:17 +0900

    add

--- src/main.go
+++ src/main.go
@@ -6,11 +6,10 @@ import (
 )
 
 func handler(writer http.ResponseWriter, _ *http.Request) {
-	fmt.Fprint(writer, "Hello World change hellooooo")
+	fmt.Fprint(writer, "HHHHHHHHello World change hellooooo")
 }
 
 func main() {
-	fmt.Println("testtesttest")
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":80", nil)
 }
